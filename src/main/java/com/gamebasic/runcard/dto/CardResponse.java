974f963e19a83fd383ecd38bc420117190cfbb71
KiShack <kimhg5603@gmail.com>
2026-09-10 18:06:53 +0900

    LV.5 작업 내용

--- src/main/java/com/gamebasic/runcard/dto/CardResponse.java
+++ src/main/java/com/gamebasic/runcard/dto/CardResponse.java
@@ -5,7 +5,15 @@ import lombok.Getter;
 @Getter
 public class CardResponse {
     // TODO (Lv 5): API 명세의 카드 응답 JSON에 맞게 필드를 만들고 생성자에서 채우세요.
+    /// LV.5 작업 내용
+    private Long id;
+    private String cardType;
+    private int acquiredFloor;
 
     public CardResponse(Long id, String cardType, int acquiredFloor) {
+        this.id = id;
+        this.cardType = cardType;
+        this.acquiredFloor = acquiredFloor;
     }
+    ///
 }
--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,18 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
     // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    /// LV.5 작업 내용
+    @NotBlank
     private String cardType;
+
+    @NotNull
+    @Min(0)
+    @Max(10)
     private Integer acquiredFloor;
+    ///
 }
